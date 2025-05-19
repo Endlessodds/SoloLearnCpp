b399a8ad1e8339d2390de30b356499514bb64bdb
Abenezer K. <156015109+Endlessodds@users.noreply.github.com>
2025-05-19 19:52:54 +0300

    Create answer.cpp

---
+++ CodingChallenges/BallparkOrders/answer.cpp
@@ -0,0 +1,19 @@
+𝐄𝐧𝐝𝐥𝐞𝐬𝐬 𝐉𝐨𝐮𝐫𝐧𝐞𝐲𝐬, [5/19/2025 7:41 PM]
+#include <iostream> 
+using namespace std; 
+ 
+int main() {
+double Nachos, Pizza, Cheeseburger, Water,Coke,Tax,subtot; 
+string order;
+Nachos = 6.00;
+Pizza = Nachos;
+Cheeseburger = 10;
+Water = 4.00;
+Coke 5.00; 
+Tax = 0.07;
+subtot;
+cout<<"Whats your order";
+cin<<order;
+
+ return 0; 
+}
